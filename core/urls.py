01fdae5490cd62c672482d7dd6edc4e2c5e8ddd3
ismaelparra123 <101906548+ismaelparra123@users.noreply.github.com>
2023-05-23 16:35:18 -0400

    commit

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
-from .views import home
+from .views import home,productos,productores,foro,login,registro
 
 urlpatterns = [
     path('',home,name="home"),
+    path('productos',productos,name="productos"),
+    path('productores',productores,name="productores"),
+    path('foro',foro,name="foro"),
+    path('login',login,name="login"),
+    path('registro',registro,name="registro"),
+    
 ]
--- core/views.py
+++ core/views.py
@@ -5,3 +5,19 @@ from django.shortcuts import render
 def home (request):
     return render(request,'core/home.html')
 
+
+def productos (request):
+    return render(request,'core/productos.html')
+
+def productores (request):
+    return render(request,'core/productores.html')
+
+def foro (request):
+    return render(request,'core/foro.html')
+
+def login (request):
+    return render(request,'core/login.html')
+
+def registro (request):
+    return render(request,'core/registro.html')
+
